635aab3a6360973dbb852c3126ad9f391b313643
likecheng <likecheng@vip.qq.com>
2018-02-23 17:16:41 +0800

    修改

--- codeGenerator.ts
+++ codeGenerator.ts
@@ -163,7 +163,7 @@ export class CodeGenerator {
      */
     entityToDatabase() {
         let ctxName = this.options.outFileName.split(".")[0];
-        let filePath = this.options.outDir + "/" + ctxName;
+        let filePath = __dirname + '/' + this.options.outDir + "/" + ctxName;
         let ctxModule = require(filePath);
         let ctxClassName = Object.keys(ctxModule)[0];
         let newCtxInstance = new ctxModule[ctxClassName];
